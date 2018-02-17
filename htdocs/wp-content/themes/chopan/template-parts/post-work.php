ee4143200c13e2da7db74af57905901e13dbd39b
Felipe Lavín Z <felipe@bloom-ux.com>
2018-02-17 13:47:26 -0300

    archivos de escritos y trabajos

--- htdocs/wp-content/themes/chopan/template-parts/post-work.php
+++ htdocs/wp-content/themes/chopan/template-parts/post-work.php
@@ -1,4 +1,4 @@
 <div class="column column-block work-post">
-	<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
+	<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
 	<span class="made-with"><?php the_field('hecho_con'); ?></span>
 </div>
--- htdocs/wp-content/themes/chopan/template-parts/post-writing.php
+++ htdocs/wp-content/themes/chopan/template-parts/post-writing.php
@@ -1,5 +1,5 @@
 <div class="column column-block writing-post">
-	<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
+	<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
 	<?php the_excerpt(); ?>
 	<time><?php echo ucwords( get_the_time('F, Y') ); ?></time>
 </div>
